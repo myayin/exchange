93deafabe16c6fe36aadbf1fba37e15aff54d7b5
MerveYayın <merve.yayin@kgteknoloji.com>
2025-06-09 15:13:22 +0300

    add swagger

--- exchange/src/main/java/com/app/exchange/dto/CurrencyConversionRequest.java
+++ exchange/src/main/java/com/app/exchange/dto/CurrencyConversionRequest.java
@@ -1,5 +1,6 @@
 package com.app.exchange.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -10,7 +11,12 @@ import java.math.BigDecimal;
 @Setter
 @Accessors(chain = true)
 public class CurrencyConversionRequest {
+    @Schema(description = "Amount to be converted", example = "100.00")
     private BigDecimal sourceAmount;
+
+    @Schema(description = "Source currency code", example = "USD")
     private String sourceCurrency;
+
+    @Schema(description = "Target currency code", example = "EUR")
     private String targetCurrency;
 }
